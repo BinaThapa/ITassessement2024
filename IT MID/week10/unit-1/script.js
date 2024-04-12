229650b8a7d104d2d4f6e69eedc59c1f3c852a8b
Binathapa <163953097+BinaThapa@users.noreply.github.com>
2024-04-12 16:00:34 +0545

    Add files via upload

---
+++ IT MID/week10/unit-1/script.js
@@ -0,0 +1,9 @@
+var int = 11;
+var float = 13.9;
+const boolean = 9>20;
+var string = "Bina Thapa";
+
+document.write(int,"<br>");
+document.write(float,"<br>");
+document.write(boolean,"<br>");
+document.write(string,"<br>");
---
+++ IT MID/week10/unit-2/script.js
@@ -0,0 +1,4 @@
+var name = prompt("Enter your Name: ");
+var id = prompt("Enter your Student id: ");
+
+alert(id + "\n" + name);
